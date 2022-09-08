a02a5d9b9545e0ac3af2f8f2ea93b54a41c10712
MercierMaxime <maxime.mercier@edu.univ-fcomte.fr>
2022-09-08 18:27:07 +0200

    Icônes + Images

---
+++ src/components/Footer.vue
@@ -0,0 +1,89 @@
+<script>
+import Logo from "./icones/Logo.vue";
+
+export default {
+  components: { Logo },
+};
+</script>
+
+
+<template>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@iconscout/unicons@3.0.6/css/line.css" />
+
+  <footer class="bg-white pt-8 sm:mt-10">
+    <div>
+      <Logo class="mt-14 ml-8"></Logo>
+    </div>
+    <div class="justify-left m-auto flex max-w-6xl flex-wrap text-gray-900">
+      <div class="w-1/2 p-5 sm:w-4 md:w-8">
+        <div class="mb-6 text-xs font-bold uppercase text-gray-900">SELL A HOME</div>
+
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Request an offer </a>
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Pricing </a>
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Reviews </a>
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Stories </a>
+      </div>
+
+      <div class="w-1/2 p-5 sm:w-4 md:w-8">
+        <div class="mb-6 text-xs font-bold uppercase text-gray-900">TERMS & PRIVACY</div>
+
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Trust & Safety </a>
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Terms of Service </a>
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Privacy Policy </a>
+      </div>
+
+      <div class="w-1/2 p-5 sm:w-4 md:w-8">
+        <div class="mb-6 text-xs font-bold uppercase text-gray-900">BUY A HOME</div>
+
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Buy </a>
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Finance </a>
+      </div>
+
+      <div class="w-1/2 p-5 sm:w-4 md:w-8">
+        <div class="mb-6 text-xs font-bold uppercase text-gray-900">ABOUT</div>
+
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Company </a>
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> How it works </a>
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Contact </a>
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Investors </a>
+      </div>
+
+      <div class="w-1/2 p-5 sm:w-4 md:w-8">
+        <div class="mb-6 text-xs font-bold uppercase text-gray-900">BUY, RENT AND SELL</div>
+
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Builder trade-up </a>
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Rent home </a>
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Builder trade-up </a>
+      </div>
+
+      <div class="w-1/2 p-5 sm:w-4 md:w-8">
+        <div class="mb-6 flex text-xs font-bold uppercase text-gray-900">RESOURCES</div>
+
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Blog </a>
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Guides </a>
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> FAQ </a>
+        <a href="#" class="my-4 block text-sm font-normal text-gray-500 duration-700 hover:text-gray-100"> Help Center </a>
+      </div>
+    </div>
+
+    <div class="pt-4">
+      <div class="m-auto flex max-w-5xl flex-col border-t border-indigo-100 px-3 pb-5 pt-5 text-sm text-gray-900 md:flex-row">
+        <div class="mt-4 text-center">©2021 Estatery. All rights reserved</div>
+        <div class="mt-8 flex flex-row justify-center">
+          <a href="#" class="w-10">
+            <i class="uil uil-facebook-f"></i>
+          </a>
+          <a href="#" class="w-10">
+            <i class="uil uil-twitter-alt"></i>
+          </a>
+          <a href="#" class="w-10">
+            <i class="uil uil-linkedin"></i>
+          </a>
+          <a href="#" class="w-10">
+            <i class="uil uil-instagram"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script>
+import Logo from "./icones/Logo.vue";
+import Navigation from "./icones/Navbar.vue";
+
+export default {
+  components: { Logo, Navigation },
+};
+</script>
+
+<template>
+  <header class="border-b-2 border-indigo-100 bg-white">
+    <div class="m-6 flex justify-between">
+      <Logo />
+      <Navigation />
+    </div>
+  </header>
+</template>
