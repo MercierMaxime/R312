885318e6c8ef3533be2af800b57c1006ca9b9584
MercierMaxime <maxime.mercier@edu.univ-fcomte.fr>
2022-09-16 10:44:33 +0200

    Définition config Formkit

--- src/pages/edit/new.vue
+++ src/pages/edit/new.vue
@@ -12,7 +12,13 @@
             <listemaisons v-bind="maison"/>
         </div>
         <div class="p-2">
-            <FormKit type="form" v-model="maison">
+            <FormKit type="form" v-model="maison"
+            :config="{
+            classes: {
+            input: 'p-1 rounded border-gray-300 shadow-sm border',
+            label: 'text-gray-600',
+ },
+ }">
                 <FormKit name="nom" label="nom"/>
                 <FormKit name="prix" label="prix" type="number"/>
                 <FormKit name="favori" label="mettre en valeur" type="checkbox"/>
