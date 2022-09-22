aab7a759ff418049e84f3db70b117a2e39148dd0
MercierMaxime <maxime.mercier@edu.univ-fcomte.fr>
2022-09-22 10:21:02 +0200

    Création page listefetch + id pour la suite

---
+++ src/pages/edit/[id].vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+    import FicheOffreMaison from "../../components/FormulaireOffreMaison.vue"
+    defineProps(["id"])
+</script>
+
+<template>
+        <FicheOffreMaison :id="id"/>
+</template>
