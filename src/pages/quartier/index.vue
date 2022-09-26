7826d1432ec9673c3616c68d37e03df816fd285f
MercierMaxime <maxime.mercier@edu.univ-fcomte.fr>
2022-09-26 10:25:41 +0200

    Mise à jour router

--- src/pages/quartier/index.vue
+++ src/pages/quartier/index.vue
@@ -1,7 +1,5 @@
 <template>
   <section>
-    <h1 class="text-2xl">
-      Bonjour<code class="font-mono text-base">/src/pages/quartier/index.vue</code>
-    </h1>
+    <h1 class="text-xl">Page Index</h1>
   </section>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import IndexView from '../pages/index.vue'
+import IndexView from '../pages/quartier/index.vue'
 import SimpleView from '../pages/simple.vue'
 import ListeView from '../pages/liste.vue'
 import ListeSupabaseView from '../pages/listeSupabase.vue'
@@ -11,11 +11,11 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         { path: '/', name: 'AppView', component: AppView },
-        { path: '/index', name: 'IndexView', component: IndexView },
+        { path: '/quartier/index', name: 'IndexView', component: IndexView },
         { path: '/liste', name: 'ListeView', component: ListeView },
         { path: '/listeSupabase', name: 'ListeSupabaseView', component: ListeSupabaseView },
         { path: '/simple', name: 'SimpleView', component: SimpleView },
-        { path: '/new', name: 'NewView', component: NewView }
+        { path: '/edit/new', name: 'NewView', component: NewView }
     ]
 })
 
