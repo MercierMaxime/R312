0b59d76910aefc17f53c2b3426a32b95885875b9
MercierMaxime <maxime.mercier@edu.univ-fcomte.fr>
2022-09-29 17:26:36 +0200

    Affichage 'listeSupabase'

--- src/pages/listeSupabase.vue
+++ src/pages/listeSupabase.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
     import { supabase } from "../supabase";
     import card from "../components/card.vue"
+
     console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
+
     let { data: Maison, error } = await supabase
+
     .from('Maison')
     .select('*') // à remplacer par l'appel à Supabase
     console.log(Maison)
@@ -11,5 +14,8 @@
 <template>
     <div class="p-2">
         <h1 class="text-xl">Page Supabase</h1>
+        <div v-for="maison in Maison" :key="maison.nomMaison">
+            <Card class="w-1/2" v-bind="maison"/>
+        </div>
     </div>
 </template>
